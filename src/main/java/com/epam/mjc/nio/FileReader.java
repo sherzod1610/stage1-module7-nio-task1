ff216de82fa130f50833bbda6156f879a0637830
Sherzod1610 <sherzodrasuljonov086@gmail.com>
2023-05-08 10:19:37 +0500

    I have finished

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        FileReader fileReader = new FileReader();
+        Profile profile = new Profile();
+        String[] array;
+        try {
+            array = fileReader.getInformation(fileReader.readFile(file));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        profile.setName(array[1]);
+        profile.setAge(Integer.valueOf(array[3]));
+        profile.setEmail(array[5]);
+        profile.setPhone(Long.valueOf(array[7]));
+        return profile;
+    }
+
+    public String readFile(File file) throws IOException {
+        StringBuilder stringFile = new StringBuilder();
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream))) {
+            while (bufferedReader.readLine()!=null){
+                stringFile.append(bufferedReader.readLine()).append(" ");
+            }
+        }
+        return stringFile.toString();
+    }
+
+
+    public String[] getInformation(String text) {
+
+        return text.split("\n");
     }
 }
